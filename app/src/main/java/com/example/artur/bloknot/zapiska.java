97054fc12a4b3474c2a0dda2028ce00bd00ae8aa
Arthur Komarov <Arthur Komarov>
2016-09-13 23:16:45 +0300

    Initial commit

---
+++ app/src/main/java/com/example/artur/bloknot/zapiska.java
@@ -0,0 +1,58 @@
+package com.example.artur.bloknot;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+/**
+ * Created by Artur on 13.09.2016.
+ */
+public class zapiska extends ActionBarActivity {
+    ImageButton lol;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        setContentView(R.layout.zapiska);
+        lol = (ImageButton) findViewById( R.id.btn1);
+        showActionBar();
+        lol.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(zapiska.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+
+
+
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.zapiska_menu, menu);
+//        return true;
+//    }
+
+
+    private void showActionBar() {
+        LayoutInflater inflator = (LayoutInflater) this
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View v = inflator.inflate(R.layout.ab_custom, null);
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(false);
+        actionBar.setDisplayShowHomeEnabled (false);
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setCustomView(v);
+    }
+}
